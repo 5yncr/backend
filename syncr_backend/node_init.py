9f1a196f49be2299d75b942a23024c502f78678e
btj12 <swimbrettjohnson@gmail.com>
2018-02-22 20:25:20 -0500

    Add default node directory and option to change it

--- syncr_backend/node_init.py
+++ syncr_backend/node_init.py
@@ -3,47 +3,59 @@ import shutil
 
 from syncr_backend import crypto_util
 
+DEFAULT_INIT_DIR = ".node/"
 
-def force_initialize_node() -> None:
+
+def force_initialize_node(init_directory: str=DEFAULT_INIT_DIR) -> None:
     """Initialize new node in .node directory
     and overwrite existing .node dir"""
 
-    if os.path.exists(".node"):
-        shutil.rmtree(".node")
+    if os.path.exists(init_directory):
+        shutil.rmtree(init_directory)
 
-    initialize_node()
-
-
-def is_node_initialized() -> bool:
-    return os.path.exists(".node")
+    initialize_node(init_directory)
 
 
-def initialize_node() -> None:
+def is_node_initialized(init_directory: str=DEFAULT_INIT_DIR) -> bool:
+    return os.path.exists(init_directory)
+
+
+def initialize_node(init_directory: str=DEFAULT_INIT_DIR) -> None:
     """Initialize new node in .node directory
     Create the private key file"""
-    if os.path.exists(".node"):
+    if os.path.exists(init_directory):
         raise FileExistsError
 
-    os.mkdir(".node")
+    os.mkdir(init_directory)
     private_key = crypto_util.generate_private_key()
-    write_private_key_to_disk(private_key)
+    write_private_key_to_disk(private_key, init_directory)
 
 
-def write_private_key_to_disk(key: crypto_util.rsa.RSAPrivateKey) -> None:
+def write_private_key_to_disk(
+    key: crypto_util.rsa.RSAPrivateKey, init_directory: str,
+) -> None:
     """Write Private Key (and public key attached) to file"""
-    if os.path.exists(".node/private_key.pem"):
+    if os.path.exists(os.path.join(init_directory, "private_key.pem")):
         raise FileExistsError
 
-    with open(".node/private_key.pem", "wb") as keyfile:
+    with open(
+        os.path.join(init_directory, "private_key.pem"),
+        "wb",
+    ) as keyfile:
         keyfile.write(crypto_util.dump_private_key(key))
         keyfile.close()
 
 
-def load_private_key_from_disk() -> crypto_util.rsa.RSAPrivateKey:
+def load_private_key_from_disk(
+    init_directory: str,
+) -> crypto_util.rsa.RSAPrivateKey:
     """Load Private Key (and public key) from file"""
     try:
 
-        with open(".node/private_key.pem", "rb") as keyfile:
+        with open(
+            os.path.join(init_directory, "private_key.pem"),
+            "rb",
+        ) as keyfile:
             return crypto_util.load_private_key(keyfile.read())
 
     except (FileNotFoundError):
--- tests/test_node_init.py
+++ tests/test_node_init.py
@@ -18,9 +18,9 @@ def test_rsa_loading_and_saving():
         os.chdir(tmpdir)
         os.mkdir(".node")
         private_key = crypto_util.generate_private_key()
-        node_init.write_private_key_to_disk(private_key)
+        node_init.write_private_key_to_disk(private_key, ".node/")
         assert crypto_util.dump_private_key(
             private_key,
         ) == crypto_util.dump_private_key(
-            node_init.load_private_key_from_disk(),
+            node_init.load_private_key_from_disk(".node/"),
         )
