cb0e5083739156afe1d3cce4046e101d8d056920
Matthew Bentley <matthew@bentley.link>
2018-03-03 20:57:27 -0500

    Fix write location, fix mypy

--- syncr_backend/drop_init.py
+++ syncr_backend/drop_init.py
@@ -3,10 +3,11 @@ import os
 from syncr_backend import crypto_util
 from syncr_backend import drop_metadata
 from syncr_backend import node_init
+from syncr_backend.constants import DEFAULT_DROP_METADATA_LOCATION
+from syncr_backend.constants import DEFAULT_FILE_METADATA_LOCATION
 
 
 def initialize_drop(directory: str) -> None:
-    start = os.getcwd()
     os.chdir(directory)
     priv_key = node_init.load_private_key_from_disk()
     node_id = crypto_util.node_id_from_public_key(priv_key.public_key())
@@ -15,7 +16,10 @@ def initialize_drop(directory: str) -> None:
         drop_name=os.path.basename(directory),
         owner=node_id,
     )
-    drop_m.write_file()
+    drop_m.write_file(
+        os.path.join(directory, DEFAULT_DROP_METADATA_LOCATION),
+    )
     for f_m in files_m.values():
-        f_m.write_file()
-    os.chdir(start)
+        f_m.write_file(
+            os.path.join(directory, DEFAULT_FILE_METADATA_LOCATION),
+        )
