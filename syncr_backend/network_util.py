15823364da3e59e48a65c2a2c75a3c5008582945
Brett Johnson <swimbrettjohnson@gmail.com>
2018-03-08 22:16:36 -0500

    Add backend request functionality

---
+++ syncr_backend/network_util.py
@@ -0,0 +1,59 @@
+import socket
+from socket import SHUT_WR
+from typing import Any
+from typing import Dict
+
+import bencode  # type: ignore
+
+from syncr_backend.constants import DEFAULT_BUFFER_SIZE
+from syncr_backend.constants import ERR_INCOMPAT
+from syncr_backend.constants import ERR_NEXIST
+
+
+class NotExistException(Exception):
+    """Requested object does not exist"""
+    pass
+
+
+class IncompatibleProtocolVersionException(Exception):
+    """Version number of the protocol not compatible with request"""
+    pass
+
+
+def send_request_to_node(
+    request: Dict[str, Any], ip: str, port: int,
+) -> bytes:
+    """
+    Creates a connection a node and sends a given request to the
+    node and returns the response
+    :param port: port where node is serving
+    :param ip: ip of node
+    :param request: Dictionary of a request as specified in the Spec Document
+    :return: node response
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((ip, port))
+        s.send(bencode.encode(request))
+        s.shutdown(SHUT_WR)
+        data = b''
+        while 1:
+            sockdata = s.recv(DEFAULT_BUFFER_SIZE)
+            if not sockdata:
+                break
+            data += sockdata
+        s.close()
+
+        response = bencode.decode(data)
+        if (response['status'] == 'ok'):
+            return bencode.decode(response['response'])
+        else:
+            exceptionmap = {
+                ERR_NEXIST: NotExistException,
+                ERR_INCOMPAT: IncompatibleProtocolVersionException,
+            }
+            raise exceptionmap[response['error']]
+
+    except socket.timeout:
+        s.close()
+        raise TimeoutError('ERROR: backend connection socket timed out')
---
+++ syncr_backend/requests.py
@@ -0,0 +1,133 @@
+from typing import List
+from typing import Optional
+
+from syncr_backend import drop_metadata
+from syncr_backend import file_metadata
+from syncr_backend import network_util
+from syncr_backend.constants import REQUEST_TYPE_CHUNK
+from syncr_backend.constants import REQUEST_TYPE_CHUNK_LIST
+from syncr_backend.constants import REQUEST_TYPE_DROP_METADATA
+from syncr_backend.constants import REQUEST_TYPE_FILE_METADATA
+from syncr_backend.drop_metadata import DropMetadata
+from syncr_backend.drop_metadata import DropVersion
+from syncr_backend.file_metadata import FileMetadata
+
+
+def send_drop_metadata_request(
+    ip: str,
+    port: int,
+    drop_id: str,
+    protocol_version: Optional[int]=1,
+    drop_version: Optional[DropVersion]=None,
+) -> DropMetadata:
+    """
+    Sends drop metadata request to node at ip and port
+    :param ip: ip address of node
+    :param port: port of the node
+    :param drop_id: drop_id of the requested drop
+    :param protocol_version: protocol_version of the request
+    :param drop_version: version of the drop
+    :return: DropMetadata object
+    """
+    request_dict = {
+        'protocol_version': protocol_version,
+        'request_type': REQUEST_TYPE_DROP_METADATA,
+        'drop_id': drop_id,
+    }
+    if drop_version is not None:
+        request_dict['version'] = drop_version.version
+        request_dict['nonce'] = drop_version.nonce
+
+    drop_metadata_bytes = network_util.send_request_to_node(
+        request_dict,
+        ip,
+        port,
+    )
+    return drop_metadata.decode(drop_metadata_bytes)
+
+
+def send_file_metadata_request(
+    ip: str,
+    port: int,
+    file_id: str,
+    protocol_version: Optional[int]=1,
+) -> FileMetadata:
+    """
+    Sends file metadata request to node at ip and port
+    :param ip: ip address of node
+    :param port: port of the node
+    :param file_id: file_id of the requested file
+    :param protocol_version: protocol_version of the request
+    :return: FileMetadata object
+    """
+    request_dict = {
+        'protocol_version': protocol_version,
+        'request_type': REQUEST_TYPE_FILE_METADATA,
+        'file_id': file_id,
+    }
+
+    file_metadata_bytes = network_util.send_request_to_node(
+        request_dict,
+        ip,
+        port,
+    )
+    return file_metadata.decode(file_metadata_bytes)
+
+
+def send_chunk_list_request(
+    ip: str,
+    port: int,
+    file_id: str,
+    protocol_version: Optional[int]=1,
+) -> List[int]:
+    """
+    Sends chunk list request to node at ip and port
+    :param ip: ip address of node
+    :param port: port of the node
+    :param file_id: file_id of the requested chunk list
+    :param protocol_version: protocol_version of the request
+    :return: List[int] of indexes of the file
+    """
+    request_dict = {
+        'protocol_version': protocol_version,
+        'request_type': REQUEST_TYPE_CHUNK_LIST,
+        'file_id': file_id,
+    }
+
+    chunk_index_list = network_util.send_request_to_node(
+        request_dict,
+        ip,
+        port,
+    )
+    return chunk_index_list
+
+
+def send_chunk_request(
+    ip: str,
+    port: int,
+    file_id: str,
+    file_index, int,
+    protocol_version: Optional[int]=1,
+) -> bytes:
+    """
+    Sends chunk request to node at ip and port
+    :param ip: ip address of node
+    :param port: port of the node
+    :param file_id: file_id of the requested chunk
+    :param file_index: index of the file for the chunk
+    :param protocol_version: protocol_version of the request
+    :return: bytes of the actual chunk
+    """
+    request_dict = {
+        'protocol_version': protocol_version,
+        'request_type': REQUEST_TYPE_CHUNK,
+        'file_id': file_id,
+        'index': file_index,
+    }
+
+    chunk = network_util.send_request_to_node(
+        request_dict,
+        ip,
+        port,
+    )
+    return chunk
