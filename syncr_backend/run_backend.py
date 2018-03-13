92fd65a139210bd0a3d744531115e450d543ea28
Brett Johnson <btj12@case.edu>
2018-03-13 18:00:58 -0400

    Fix sys.exit being called by argparse exiting the program

--- syncr_backend/run_backend.py
+++ syncr_backend/run_backend.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 import argparse
 import sys
-from typing import List
-from typing import Tuple
 import threading
+from typing import Any
+from typing import List
+
 from syncr_backend.init import drop_init
 from syncr_backend.init import node_init
-from syncr_backend.network.listen_requests import listen_requests
 from syncr_backend.network import send_requests
+from syncr_backend.network.listen_requests import listen_requests
 
 
 def run_backend() -> None:
@@ -34,6 +35,8 @@ def run_backend() -> None:
         args=[arguments.ip[0], arguments.port[0]],
     )
     request_listen_thread.start()
+    read_cmds_from_cmdline()
+    request_listen_thread.join()
 
 
 def read_cmds_from_cmdline() -> None:
@@ -42,19 +45,34 @@ def read_cmds_from_cmdline() -> None:
     """
     exit = False
     while not exit:
-        command = input("5yncr >>> ")
-        function_name, args = parse_cmd(command)
+        command = input("5yncr >>> ").split(' ')
+        return_list = ["", []]
+        parse_cmd_thread = threading.Thread(
+            target=parse_cmd,
+            args=[command, return_list],
+        )
+        parse_cmd_thread.start()
+        parse_cmd_thread.join()
+        function_name = str(return_list[0])
+        args = list(return_list[1])
+
+        if function_name is None:
+            continue
         if function_name != 'exit':
             execute_function(function_name, args)
         else:
             exit = True
 
 
-def parse_cmd(command: str) -> Tuple[str, List[str]]:
+def parse_cmd(
+    command: List[str],
+    return_list: List[Any],
+) -> None:
     """
     Parse command given in arguments
     :param command: str command to run
-    :return: Tuple of function name and argumen
+    :param return_list: for when calling in a thread, the return list is
+    modified to be the return value of the function
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -71,10 +89,11 @@ def parse_cmd(command: str) -> Tuple[str, List[str]]:
         help='arguments for the function to be ran',
     )
     args = parser.parse_args(args=command)
+    return_list[0] = args.function[0]
     if args.args is not None:
-        return (args.function[0], args.args)
+        return_list[1] = args.args
     else:
-        return (args.function[0], [])
+        return_list[1] = []
 
 
 def execute_function(function_name: str, args: List[str]):
@@ -82,22 +101,27 @@ def execute_function(function_name: str, args: List[str]):
     Runs a function with the given args
     TODO: add real drop/metadata request commands that interface
     with the filesystem
+    TODO: handle exceptions for real
 
     :param function_name: string name of the function to run
     :param args: arguments for the function to run
     """
-
+    found_function = False
     # for functions that create or destroy the init directory
     if function_name == "node_init":
         node_init.initialize_node(*args)
+        found_function = True
     elif function_name == "node_force_init":
-        node_init.force_initialize_node(args[0])
+        node_init.force_initialize_node(*args)
+        found_function = True
     elif function_name == "delete_node":
-        node_init.delete_node_directory(args[0])
+        node_init.delete_node_directory(*args)
+        found_function = True
 
     # drop functions
     if function_name == "drop_init":
         drop_init.initialize_drop(args[0])
+        found_function = True
 
     # request functions, only for debug
     try:
@@ -107,18 +131,21 @@ def execute_function(function_name: str, args: List[str]):
                 int(args[1]),
                 args[2],
             ))
+            found_function = True
         elif function_name == "send_file_metadata_request":
             print(send_requests.send_file_metadata_request(
                 args[0],
                 int(args[1]),
                 args[2],
             ))
+            found_function = True
         elif function_name == "send_chunk_list_request":
             print(send_requests.send_chunk_list_request(
                 args[0],
                 int(args[1]),
                 args[2],
             ))
+            found_function = True
         elif function_name == "send_chunk_request":
             print(send_requests.send_chunk_request(
                 args[0],
@@ -126,10 +153,14 @@ def execute_function(function_name: str, args: List[str]):
                 args[2],
                 int(args[4]),
             ))
+            found_function = True
     # placeholder only for debugging
     except Exception:
         print("Error in handling request function")
         print(str(sys.exc_info()))
+
+    if not found_function:
+        print("Function [%s] not found" % (function_name))
 
 
 if __name__ == '__main__':
