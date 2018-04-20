ca34d5ead91d35db2bc5b713afd7a12981ca8d92
Jack LaRue <jvl13@case.edu>
2018-04-20 01:43:59 -0400

    Init handle_frontend.py unit testing
    Signed-off-by: Jack LaRue <jvl13@case.edu>

--- tests/test_handle_frontend.py
+++ tests/test_handle_frontend.py
@@ -1,30 +1,161 @@
-"""
-from typing import Any
+from unittest import mock
+from unittest.mock import MagicMock
+from unittest.mock import Mock
 
-# The following are functionalities are needed.
-from syncr_backend.network.handle_frontend import handle_transfer_ownership
-from syncr_backend.network.handle_frontend import handle_add_file
-from syncr_backend.network.handle_frontend import handle_add_owner
-from syncr_backend.network.handle_frontend import handle_delete_drop
-from syncr_backend.network.handle_frontend import handle_get_owned_drops
-from syncr_backend.network.handle_frontend import handle_get_selected_drops
-from syncr_backend.network.handle_frontend import handle_get_subscribed_drops
-from syncr_backend.network.handle_frontend import handle_input_subscribe_drop
-from syncr_backend.network.handle_frontend import handle_initialize_drop
-from syncr_backend.network.handle_frontend import handle_remove_file
-from syncr_backend.network.handle_frontend import handle_remove_owner
-from syncr_backend.network.handle_frontend import handle_share_drop
-from syncr_backend.network.handle_frontend import handle_unsubscribe
+from syncr_backend.constants import ACTION_ADD_FILE
+from syncr_backend.constants import ACTION_ADD_OWNER
+from syncr_backend.constants import ACTION_DELETE_DROP
+from syncr_backend.constants import ACTION_GET_OWNED_DROPS
+from syncr_backend.constants import ACTION_GET_SELECT_DROPS
+from syncr_backend.constants import ACTION_GET_SUB_DROPS
+from syncr_backend.constants import ACTION_INITIALIZE_DROP
+from syncr_backend.constants import ACTION_INPUT_DROP_TO_SUBSCRIBE_TO
+from syncr_backend.constants import ACTION_REMOVE_FILE
+from syncr_backend.constants import ACTION_REMOVE_OWNER
+from syncr_backend.constants import ACTION_SHARE_DROP
+from syncr_backend.constants import ACTION_TRANSFER_OWNERSHIP
+from syncr_backend.constants import ACTION_UNSUBSCRIBE
+from syncr_backend.network.handle_frontend import handle_frontend_request
+
+# Bear in mind, the only functions that are going to be tested are the
+# ones with full backend functionality by the time the final commit is due.
+# Some functions have been left intentionally untested until they
+# have been fully implemented.
 
 
-# The following are not
-from syncr_backend.network.handle_frontend import handle_accept_changes
-from syncr_backend.network.handle_frontend import handle_accept_conflict_file
-from syncr_backend.network.handle_frontend import handle_decline_changes
-from syncr_backend.network.handle_frontend import handle_decline_conflict_file
-from syncr_backend.network.handle_frontend import handle_get_conflicting_files
-from syncr_backend.network.handle_frontend import handle_request_change
-from syncr_backend.network.handle_frontend import handle_view_conflicts
-from syncr_backend.network.handle_frontend import handle_view_pending_changes
+def test_handle_frontend_request():
 
-"""
+    conn = Mock()
+    conn.send = MagicMock()
+
+    with mock.patch(
+        'syncr_backend.network.handle_frontend.handle_add_file',
+        autospec=True,
+    ) as mock_handle_add_file, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_add_owner',
+        autospec=True,
+    ) as mock_handle_add_owner, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_delete_drop',
+        autospec=True,
+    ) as mock_handle_delete_drop, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_get_owned_drops',
+        autospec=True,
+    ) as mock_handle_get_owned_drops, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_get_selected_drops',
+        autospec=True,
+    ) as mock_handle_get_selected_drops, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_get_subscribed_drops',
+        autospec=True,
+    ) as mock_handle_get_subscribed_drops, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_input_subscribe_drop',
+        autospec=True,
+    ) as mock_handle_input_subscribe_drop, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_initialize_drop',
+        autospec=True,
+    ) as mock_handle_initialize_drop, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_remove_file',
+        autospec=True,
+    ) as mock_handle_remove_file, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_remove_owner',
+        autospec=True,
+    ) as mock_handle_remove_owner, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_share_drop',
+        autospec=True,
+    ) as mock_handle_share_drop, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_transfer_ownership',
+        autospec=True,
+    ) as mock_handle_transfer_ownership, mock.patch(
+        'syncr_backend.network.handle_frontend.handle_unsubscribe',
+        autospec=True,
+    ) as mock_handle_unsubscribe:
+
+        request = {
+            'action': ACTION_ADD_FILE,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_add_file.assert_called_once()
+
+        request = {
+            'action': ACTION_ADD_OWNER,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_add_owner.assert_called_once()
+
+        request = {
+            'action': ACTION_DELETE_DROP,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_delete_drop.assert_called_once()
+
+        request = {
+            'action': ACTION_GET_OWNED_DROPS,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_get_owned_drops.assert_called_once()
+
+        request = {
+            'action': ACTION_GET_SUB_DROPS,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_get_subscribed_drops.assert_called_once()
+
+        request = {
+            'action': ACTION_GET_SELECT_DROPS,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_get_selected_drops.assert_called_once()
+
+        request = {
+            'action': ACTION_INPUT_DROP_TO_SUBSCRIBE_TO,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_input_subscribe_drop.assert_called_once()
+
+        request = {
+            'action': ACTION_INITIALIZE_DROP,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_initialize_drop.assert_called_once()
+
+        request = {
+            'action': ACTION_REMOVE_FILE,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_remove_file.assert_called_once()
+
+        request = {
+            'action': ACTION_REMOVE_OWNER,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_remove_owner.assert_called_once()
+
+        request = {
+            'action': ACTION_SHARE_DROP,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_share_drop.assert_called_once()
+
+        request = {
+            'action': ACTION_TRANSFER_OWNERSHIP,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_transfer_ownership.assert_called_once()
+
+        request = {
+            'action': ACTION_UNSUBSCRIBE,
+        }
+
+        handle_frontend_request(request, conn)
+        mock_handle_unsubscribe.assert_called_once()
